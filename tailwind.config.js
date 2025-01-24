af469a1c4425444b98bc44ba54c8d24eda462448
Ifeanyichukwu Favour <ifeanyifavour74@gmail.com>
2025-01-24 15:27:01 +0100

    Defined structure

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    colors: {
+      white: "#FFFFFF",
+      "neutral-200": "#CFD2D1",
+      "neutral-500": "#697170",
+      "neutral-800": "#434747",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
